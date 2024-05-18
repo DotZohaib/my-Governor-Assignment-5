645aca078947d12ffa6c622770c35bd60c42e026
ZOHAIB ALI DAYO <154052553+DotZohaib@users.noreply.github.com>
2024-05-19 00:44:05 +0500

    Add files via upload
    my Governor Assignment #5       using JavaScript

---
+++ app.js
@@ -0,0 +1,198 @@
+// // function of js
+
+// function city(){
+//     console.log("pak");
+//     return "Aus"
+// }
+
+
+// const ans = city();
+// console.log(ans);
+
+
+
+// // arrow functionality
+
+// const hello = () =>{
+// console.log("Hello from Arrow function")
+// }
+
+// hello();
+
+
+
+// let name = "IsEven";
+// let digit = 5
+
+// if (digit % 2 == 0){
+//     console.log("Even" )
+// }else{
+//     console.log("Odd")
+// }
+
+// let result = () => {
+//     if (digit % 2 == 0){
+//         console.log("Even" )
+//     }else{
+//         console.log("Odd")
+//     }
+// }
+// result(20)
+
+
+
+// return with single statement
+
+// const Name =  () => "Power"
+
+// const ans = Name();
+// console.log(ans)
+
+
+// const checkName = (name) => {
+//     if (name === "Zohaib") {
+//       console.log(true);
+//     } else {
+//       console.log(false);
+//     }
+//   };
+
+//   checkName("Zohaib");
+//   checkName("Ali");  // This will print false
+
+
+
+// const Check = (name) => {
+//     if (name === "KPS") {
+//       console.log(" this area is my under");
+//     } else {
+//       console.log("this is not my area");
+//     }
+//   };
+
+//  Check("KPS");
+// const Check1 = (name) => {
+//     if (name === "SRF") {
+//       console.log(" this area is my under");
+//     } else {
+//       console.log("this is not my area");
+//     }
+//   };
+
+//  Check1("SRF1");
+
+// let teacher = "Ameen"
+// function ZohaibSession() {
+//     let teacher = "Zohaib"
+//     console.log(teacher)
+// }
+
+// ZohaibSession();
+
+
+// let myArray = [{name: "Zohaib", age: 20}, {name: "Ark", age: 19}]
+
+// console.log(myArray [1])
+
+// myArray.pop("ark");
+// myArray.push("Khan")
+// myArray.shift("ark");
+// myArray.unshift("ark");
+
+// create the array?
+
+
+
+
+// let array = ["banana","apple", "mango", "grapes" ];
+
+// array.pop();
+// array.push("grapes");
+// array.push("grapes", "gree");
+// array.shift();
+// array.unshift("grapes");
+// array.unshift("grapes", "bada");
+
+
+// let myArray = array.slice(1,2);
+// let myArray = array.slice(-2);
+
+
+// array.splice(0,2, "item", "peach")
+
+// array.splice(0,0, "item", "peach")
+
+
+// console.log(array);
+
+
+
+
+// Object and key
+
+// const myCar ={
+//     Brand: "tesla",
+//     Model: "model 3",
+//     color: "red",
+//     start: () => {
+//         return "Max Speed 500"
+//     }
+
+// }
+
+// console.log(myCar.start());
+
+// console.log(myCar.Brand);
+// console.log(myCar.Model);
+// console.log(myCar.color);
+
+
+
+// const myCar =[{
+//     Brand: "tesla",
+//     Model: "model 3",
+//     color: "red"
+
+// }
+// ,{
+//     Brand: "MX",
+//     Model: "model 7",
+//     color: "white"
+
+// }
+// ]
+// console.log(myCar);
+
+
+
+const info = [{
+    Name: "Zohaib",
+    Email: "zealib@gmail.com",
+    ID: 1234567
+},{
+    Name: "Zohaib",
+    Email: "zealib@gmail.com",
+    ID: 1234567
+},{
+    Name: "Zohaib",
+    Email: "zealib@gmail.com",
+    ID: 1234567
+},{
+    Name: "Zohaib",
+    Email: "zealib@gmail.com",
+    ID: 1234567
+},{
+    Name: "Zohaib",
+    Email: "zealib@gmail.com",
+    ID: 1234567
+},{
+    Name: "Zohaib",
+    Email: "zealib@gmail.com",
+    ID: 1234567
+},]
+
+console.log(info);
+
+// console.log(info.Name);
+// console.log(info.Email);
+// console.log(info.ID);
